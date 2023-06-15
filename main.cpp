7e6e325ffe162d2bd2d0317ef5c4f2e9f812f70a
yermushy31 <hector.caillebottevince@limayrac.fr>
2023-06-15 11:01:52 +0200

    commit client

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+class Client {
+public:
+    Client(const std::string& serverIP, int serverPort)
+            : serverIP(serverIP), serverPort(serverPort), clientSocket(INVALID_SOCKET) {}
+
+    bool Connect() {
+        WSADATA wsData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0) {
+            std::cout << "Failed to initialize Winsock." << std::endl;
+            return false;
+        }
+
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cout << "Failed to create socket." << std::endl;
+            WSACleanup();
+            return false;
+        }
+
+        sockaddr_in serverAddress{};
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(serverPort);
+        if (inet_pton(AF_INET, serverIP.c_str(), &(serverAddress.sin_addr)) <= 0) {
+            std::cout << "Invalid address or address not supported." << std::endl;
+            closesocket(clientSocket);
+            WSACleanup();
+            return false;
+        }
+
+        if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+            std::cout << "Failed to connect to the server." << std::endl;
+            closesocket(clientSocket);
+            WSACleanup();
+            return false;
+        }
+
+        return true;
+    }
+
+    void Run() {
+        std::cout << "Connected to the server. Enter 'exit' to quit." << std::endl;
+
+        std::string input;
+        char buffer[4096];
+        int bytesRead;
+
+        while (true) {
+            std::cout << "Client~# ";
+            std::getline(std::cin, input);
+
+            if (input == "exit") {
+                break;
+            }
+
+            if (send(clientSocket, input.c_str(), input.length(), 0) == SOCKET_ERROR) {
+                std::cout << "Failed to send data to the server." << std::endl;
+                break;
+            }
+
+            bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+            if (bytesRead == SOCKET_ERROR) {
+                std::cout << "Failed to receive data from the server." << std::endl;
+                break;
+            }
+
+            buffer[bytesRead] = '\0';
+            std::cout << "Server response: " << buffer << std::endl;
+        }
+
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+private:
+    std::string serverIP;
+    int serverPort;
+    SOCKET clientSocket;
+};
+
+int main() {
+    std::string serverIP = "127.0.0.1";
+    int serverPort = 5555;  // 5555 / 4444
+
+    Client client(serverIP, serverPort);
+    if (client.Connect()) {
+        client.Run();
+    }
+
+    return 0;
+}
