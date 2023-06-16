bb76b7c424c86790813e434d67a53fec0e83e6de
yermushy31 <hector.caillebottevince@limayrac.fr>
2023-06-16 14:16:55 +0200

    server commit

---
+++ encrypt.h
@@ -0,0 +1,35 @@
+//
+// Created by yermushy on 16/06/2023.
+// serversocket
+
+#ifndef UNDERCOVERSERVER_ENCRYPT_H
+#define UNDERCOVERSERVER_ENCRYPT_H
+#include <iostream>
+#include <WinSock2.h>
+#include <vector>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+class Encryption {
+public:
+    Encryption(SOCKET clientsock, SOCKET serversock);
+    ~Encryption();
+    void CheckSslVersion(void);
+    void InitSSL(void);
+    bool CreateSSLContext();
+    bool LoadSSLCertificate();
+    bool LoadSSLPrivateKey();
+    bool CreateSSLObject();
+    bool BindSSL();
+    bool SSLHandshake();
+    void SendSSL(const char* buffer);
+private:
+    std::vector<SSL*> connections;
+    SSL_CTX* SSLContext = nullptr;
+    const char* Certificate = nullptr;
+    const char* PrivateKey = nullptr;
+    SOCKET Clientsock;
+    SOCKET Serversock;
+
+};
+#endif //UNDERCOVERSERVER_ENCRYPT_H
